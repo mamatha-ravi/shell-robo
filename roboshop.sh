117b12efbac542215721f21d1716572b7bd56d15
mamatha-ravi <mamata.r@gmail.com>
2026-01-30 15:23:36 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 Userid=$(id -u)
-
+SG_ID="sg-01d73528856414fb1"
+AMI_ID="ami-0220d79f3f480ecf5"
+DOMAIN_NAME="devops88s.online"
 R='\e[31m'
 G='\e[32m'
 Y='\e[33m'
@@ -13,13 +15,41 @@ if [ $Userid -ne 0 ]; then
 echo -e "$R this is not sudo user $N" | tee -a $log_file
 exit 1
 fi
-mkdir -p $log_folder
-# Package=$1
-validate (){
-    if [ $1 -eq 0 ]; then 
-echo -e "$Package installation $G Success $N" | tee -a $log_file
-else 
-echo -e "$Package installation $R failure $N" | tee -a $log_file
-fi
-}
+# mkdir -p $log_folder
+# # Package=$1
+# validate (){
+#     if [ $1 -eq 0 ]; then 
+# echo -e "$Package installation $G Success $N" | tee -a $log_file
+# else 
+# echo -e "$Package installation $R failure $N" | tee -a $log_file
+# fi
+# }
+for instance in $@
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro" \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
 
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        # RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        # RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
+    fi
+
+echo "IP address $IP"
