9b2da4f00ced21cbfb232458bfe1c88de94c6ef8
mamatha-ravi <mamata.r@gmail.com>
2026-01-30 15:30:24 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -9,12 +9,12 @@ Y='\e[33m'
 B='\e[34m'
 N='\e[0m'   # No Color
 
-log_folder="/var/log/shellscript"
-log_file="/var/log/shellscript/$0.log"
-if [ $Userid -ne 0 ]; then
-echo -e "$R this is not sudo user $N" | tee -a $log_file
-exit 1
-fi
+# log_folder="/var/log/shellscript"
+# log_file="/var/log/shellscript/$0.log"
+# if [ $Userid -ne 0 ]; then
+# echo -e "$R this is not sudo user $N" | tee -a $log_file
+# exit 1
+# fi
 # mkdir -p $log_folder
 # # Package=$1
 # validate (){
