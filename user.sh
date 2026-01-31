da840728b2db7f89900d1d4250ed35f044914e5c
mamatha-ravi <mamata.r@gmail.com>
2026-01-31 20:18:51 +0530

    shellrobo

---
+++ user.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+Userid=$(id -u)
+SCRIPT_DIR=$PWD
+mongodb_host="mongodb.devops88sonline.com"
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+N='\e[0m'   # No Color
+
+log_folder="/var/log/roboshop"
+log_file="$log_folder/$0.log"
+if [ $Userid -ne 0 ]; then
+echo -e "$R this is not sudo user $N" | tee -a $log_file
+exit 1
+fi
+mkdir -p $log_folder
+# Package=$1
+validate (){
+    if [ $? -eq 0 ]; then 
+echo -e "$1.. $G Success $N" | tee -a $log_file
+else 
+echo -e "$1.. $R failure $N" | tee -a $log_file
+fi
+}
+
+if command -v node &>/dev/null; then
+  echo "Node.js already installed"
+else
+  echo "Installing Node.js"
+ 
+dnf module enable nodejs:20 -y &>>$log_file
+validate "enabling nodejs"
+
+dnf install nodejs -y &>>$log_file
+validate "installing nodejs"
+fi
+id roboshop 
+if [ $? -ne 0 ]; then
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+validate "system user adding"
+else
+echo -e "$Y roboshop user already exists..skipping $N"
+fi
+
+mkdir -p /app 
+validate "creating directory"
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip  &>>$log_file
+validate "downloading user code"
+cd /app 
+validate "inside app dictory"
+rm -rf /app/* &>>$log_file
+validate "removing existing files"
+
+unzip /tmp/user.zip &>>$log_file
+validate "unzip user code"
+
+npm install &>>$log_file
+validate "installing dependencies"
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+validate "created systemctl service"
+
+systemctl enable user 
+systemctl start user
+validate "enabling and starting user"
