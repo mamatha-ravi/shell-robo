ffc5f6328346100d89612e753644f0343dca0818
mamatha-ravi <mamata.r@gmail.com>
2026-01-31 17:58:46 +0530

    shellrobo

--- roboshop.sh
+++ roboshop.sh
@@ -28,7 +28,7 @@ N='\e[0m'   # No Color
 # }
 for instance in $@
 do
-    INSTANCE_ID=$( aws ec2 run-instances \
+    INSTANCE_ID=$( /usr/local/bin/aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
